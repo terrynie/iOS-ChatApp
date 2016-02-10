491866962cb76b89acbb8de9cabe9b4e91166030
terry408911 <519046658@qq.com>
2016-02-10 23:19:32 +0800

    init

---
+++ iOS-ChatApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-ChatApp
+//
+//  Created by Terry on 2/10/16.
+//  Copyright © 2016 Terry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS-ChatApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOS-ChatApp
+//
+//  Created by Terry on 2/10/16.
+//  Copyright © 2016 Terry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
